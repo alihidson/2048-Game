9c6c5471d03dc79ab100281ef64f47ddfd836992
alihidson <amakhavan81@gmail.com>
2024-11-27 17:16:04 +0330

    change my idea for linkedList and then i want to use one doubly-linkedList for save nodes and rewrite functions of linkedList class and node class and my main and check code to run and this code is successfully runed

--- linkedList.py
+++ linkedList.py
@@ -2,104 +2,44 @@ from node import Node
 
 class LinkedList:
     def __init__(self):
-        self.head = Node(0, -1) # create head
         
-        # create nodes for rows
-        self.row1 = Node(0, -1)
-        self.head.down = self.row1
-        self.row1.up = self.head
+        self.head = Node(0, -1) # the number is 0 and the value is -1
         
-        self.row2 = Node(0, -1)
-        self.row1.down = self.row2
-        self.row2.up = self.row1
-        
-        self.row3 = Node(0, -1)
-        self.row2.down = self.row3
-        self.row3.up = self.row2
-        
-        self.row4 = Node(0, -1)
-        self.row3.down = self.row4
-        self.row4.up = self.row3
-        
-        
-        # create nodes for columns
-        self.col1 = Node(0, -1)
-        self.head.next = self.col1
-        self.col1.prev = self.head
-        
-        self.col2 = Node(0, -1)
-        self.col1.next = self.col2
-        self.col2.prev = self.col1
-        
-        self.col3 = Node(0, -1)
-        self.col2.next = self.col3
-        self.col3.prev = self.col2
-        
-        self.col4 = Node(0, -1)
-        self.col3.next = self.col4
-        self.col4.prev = self.col3
         
         
         
 
     def add_node(self, number, value):
+        new_Node = Node(number, value)
         
-        newNode = Node(number, value)
-        current = self.row1
+        if self.head.next == None:
+            self.head.next = new_Node
+            new_Node.prev = self.head
+            
+        else:
+            current = self.head.next
+            while current.next != None and current.next.number < number:
+                current = current.next
+            
+            new_Node.next = current.next
+            new_Node.prev = current
+            current.next = new_Node
+            new_Node.next.prev = new_Node
+            
+            
+    
+            
+            
         
-        if 1 <= number and number <= 4:
-            if current.next == None:
-                current.next = newNode
-                
-            else:
-                while current.next != None and current.next.number < newNode.number:
-                    current = current.next
-                    
-                newNode.next = current.next
-                current.next = newNode
-                
-                
-        elif 5 <= number and number <= 8:
-            current = self.head.down
-            
-            if current.next == None:
-                current.next = newNode
-                
-            else:
-                while current.next != None and current.next.number < newNode.number:
-                    current = current.next
-                    
-                newNode.next = current.next
-                current.next = newNode
-                
-                
-        elif 9 <= number and number <= 12:
-            current = self.head.down.down
-            
-            if current.next == None:
-                current.next = newNode
-                
-            else:
-                while current.next != None and current.next.number < newNode.number:
-                    current = current.next
-                    
-                newNode.next = current.next
-                current.next = newNode
-                
-                
-        elif 13 <= number and number <= 16:
-            current = self.head.down.down.down
-            
-            if current.next == None:
-                current.next = newNode
-                
-            else:
-                while current.next != None and current.next.number < newNode.number:
-                    current = current.next
-                    
-                newNode.next = current.next
-                current.next = newNode
-                
+        
+        
+        
+        
+        
+        
+        
+        
+        
     
     
     
@@ -108,78 +48,14 @@ class LinkedList:
     
     def check_node_empty(self, input_number):
         
-        if 1 <= input_number and input_number <= 4:
-            current = self.head.next
-            
-            sw = 1
-            while current != None:
-                
-                if current.number == input_number:
-                    sw = 0
-                current = current.next
-            
-            if sw == 0:
-                return False
-            else:
-                return True
+        sw = 1
+        current = self.head.next
         
+        while current != None:
+            if current.number == input_number:
+                sw = 0
         
-        
-        
-        if 5 <= input_number and input_number <= 8:
-            current = self.head.down.next
-            
-            sw = 1
-            while current != None:
-                
-                if current.number == input_number:
-                    sw = 0
-                current = current.next
-            
-            if sw == 0:
-                return False
-            else:
-                return True
-            
-            
-        if 9 <= input_number and input_number <= 12:
-            current = self.head.down.down.next
-            
-            sw = 1
-            while current != None:
-                
-                if current.number == input_number:
-                    sw = 0
-                current = current.next
-            
-            if sw == 0:
-                return False
-            else:
-                return True
-            
-            
-        
-        if 13 <= input_number and input_number <= 16:
-            current = self.head.down.down.down.next
-            
-            sw = 1
-            while current != None:
-                
-                if current.number == input_number:
-                    sw = 0
-                current = current.next
-            
-            if sw == 0:
-                return False
-            else:
-                return True
-        
-        
-
-# def move_right(self):
-#     current = self.head.next
-    
-#     if current.value == current.next.value:
-#         current.next.value += current.value
-        
-    
+        if sw == 0:
+            return False
+        else:
+            return True
--- node.py
+++ node.py
@@ -1,9 +1,9 @@
 class Node:
     def __init__(self, number, value):
+        
         self.number = number
         self.value = value
-        self.up = None
-        self.down = None
-        self.prev = None
+        
         self.next = None
+        self.prev = None
     
